a32d297c1db8543c1d31411beaf1780af961f2be
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2017-09-22 19:38:51 +1000

    Added tab-bar icons

--- src/views/book/index.js
+++ src/views/book/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'react-native'
 import { TabNavigator, StackNavigator } from 'react-navigation'
+import { Icon } from 'react-native-elements'
 import AllBooksScreen from './tabs/all-books'
 import FictionBooksScreen from './tabs/fiction-books'
 import NonFictionBooksScreen from './tabs/non-fiction-books'
@@ -9,12 +10,36 @@ const BookListView = TabNavigator(
   {
     AllBooksTab: {
       screen: AllBooksScreen,
+      navigationOptions: {
+        headerMode: 'none',
+        tabBarLabel: 'All Books',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Icon
+            name={focused ? 'book-open' : 'book'}
+            size={30}
+            type="material-community"
+            color={tintColor}
+          />
+        ),
+      },
     },
     FictionBooksTab: {
       screen: FictionBooksScreen,
+      navigationOptions: {
+        tabBarLabel: 'Fiction',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Icon name="clapperboard" size={30} type="entypo" color={tintColor} />
+        ),
+      },
     },
     NonFictionBooksTab: {
       screen: NonFictionBooksScreen,
+      navigationOptions: {
+        tabBarLabel: 'Nonfiction',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Icon name="book" size={30} type="font-awesome" color={tintColor} />
+        ),
+      },
     },
   },
   {
