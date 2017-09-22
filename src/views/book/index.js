3bba252555f9f7bbfc836537f3d10fbce2cca269
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2017-09-22 20:24:11 +1000

    Added tab-bar icons

--- src/views/book/index.js
+++ src/views/book/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'react-native'
 import { TabNavigator, StackNavigator } from 'react-navigation'
+import { Icon } from 'native-base'
 import AllBooksScreen from './tabs/all-books'
 import FictionBooksScreen from './tabs/fiction-books'
 import NonFictionBooksScreen from './tabs/non-fiction-books'
@@ -9,12 +10,45 @@ const BookListView = TabNavigator(
   {
     AllBooksTab: {
       screen: AllBooksScreen,
+      navigationOptions: {
+        headerMode: 'none',
+        tabBarLabel: 'All Books',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Icon
+            android="md-book"
+            ios={focused ? 'ios-book' : 'ios-book-outline'}
+            style={{ fontSize: 20, color: tintColor }}
+          />
+        ),
+      },
     },
     FictionBooksTab: {
       screen: FictionBooksScreen,
+      navigationOptions: {
+        tabBarLabel: 'Fiction',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Icon
+            android="md-ionitron"
+            ios={focused ? 'ios-ionitron' : 'ios-ionitron-outline'}
+            name="md-menu"
+            style={{ fontSize: 20, color: tintColor }}
+          />
+        ),
+      },
     },
     NonFictionBooksTab: {
       screen: NonFictionBooksScreen,
+      navigationOptions: {
+        tabBarLabel: 'Nonfiction',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Icon
+            android="md-planet"
+            ios={focused ? 'ios-planet' : 'ios-planet-outline'}
+            name="md-menu"
+            style={{ fontSize: 20, color: tintColor }}
+          />
+        ),
+      },
     },
   },
   {
